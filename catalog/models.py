13f89be8d260077c5453e716f5f8f4fdbfb95636
Imprud <imprud2@gmail.com>
2019-07-04 15:59:31 +0300

    changed catalog models, header menu changed

--- catalog/models.py
+++ catalog/models.py
@@ -1,27 +1,33 @@
 from django.db import models
+from slugify import slugify
 
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    slug = models.CharField(max_length=255, unique=True)
-    description = models.CharField(max_length=255)
-    text = models.TextField()
+    slug = models.CharField(max_length=255, blank=True, unique=True)
+    description = models.CharField(max_length=255, blank=True)
+    text = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
 
 
 class Agency(models.Model):
     name = models.CharField(max_length=255)
     slug = models.CharField(max_length=255, unique=True)
-    image = models.ImageField()
+    image = models.ImageField(blank=True)
     cats = models.ManyToManyField(Category)
-    description = models.TextField()
-    rates = models.IntegerField()
-    location = models.CharField(max_length=255)
-    year = models.IntegerField()
-    employees = models.CharField(max_length=255)
-    services = models.TextField()
+    description = models.TextField(blank=True)
+    rates = models.IntegerField(blank=True)
+    location = models.CharField(max_length=255, blank=True)
+    year = models.IntegerField(blank=True)
+    employees = models.CharField(max_length=255, blank=True)
+    services = models.TextField(blank=True)
     scores = models.CharField(max_length=255, default='5')
 
     def __str__(self):
@@ -31,9 +37,9 @@ class Agency(models.Model):
 class Review(models.Model):
     agency = models.ForeignKey(Agency, on_delete=models.CASCADE)
     nickname = models.CharField(max_length=255)
-    summary = models.CharField(max_length=255)
-    review = models.TextField()
-    score = models.TextField(max_length=255)
+    summary = models.CharField(max_length=255, blank=True)
+    review = models.TextField(blank=True)
+    score = models.TextField(max_length=255, default='5', blank=True)
 
     def __str__(self):
         return self.name
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Category
 
 
 def index(request):
-    response = render(request, 'index.html')
+    context = {
+        'categories': Category.objects.all()[:12]
+    }
+    response = render(request, 'index.html', context)
     return response
 
 
